7ff9d8ed37b981b2a695d1ed45c5979f52bfa979
BEN YOUSSEF, Elyes <elyes.by@gmail.com>
2021-02-26 17:12:14 -0500

    preloadingStrategy: PreloadAllModules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule],
   providers: []
 })
