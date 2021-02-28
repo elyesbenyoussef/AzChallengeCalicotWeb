349c2b1821af6861590d495ddf607fbb304b8f81
BEN YOUSSEF, Elyes <elyes.by@gmail.com>
2021-02-27 19:51:19 -0500

    http headers

--- src/app/areas/accueil/accueil.service.ts
+++ src/app/areas/accueil/accueil.service.ts
@@ -1,3 +1,4 @@
+import { HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ProductEntity } from "src/app/entities/product.entity";
 import { HttpClientService } from "src/app/services/http-client.service";
@@ -8,12 +9,21 @@ import { environment } from "src/environments/environment";
 })
 
 export class AccueilService {
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Request-Headers': '*',
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+        })
+    };
+
     constructor(private http: HttpClientService) {
 
     }
 
     GetAllProducts() {
-        return this.http.get(environment.ApiUrl + 'api/collection/all');
+        return this.http.get(environment.ApiUrl + 'api/collection/all', this.httpOptions);
     }
 
     AddProduct(product: ProductEntity) {
